2dcb6c83b9acdffa0f90ec7dda66c9107f9800e4
Florin Ruscuta <ruscutaf@gmail.com>
2023-02-19 11:44:08 +0200

    Add project files.

---
+++ ProiectInfo/Evaluare.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+
+
+namespace ProiectInfo
+{
+    class Evaluare
+    {
+        public int Dimensiune(string expresie)
+        {
+            return expresie.Length;
+        }
+
+        public int NrCuv(string expresie)
+        {
+            char[] separators = { ',', '.', ' ', '!', '?', '-', '+', '*', '/', '%', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] nr = expresie.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            return nr.Length;
+        }
+
+        public int NrLitere(string expresie)
+        {
+            int cnt = 0;
+            for (int i = 0; i < expresie.Length; ++i)
+                if ((expresie[i] >= 'a' && expresie[i] <= 'z') || (expresie[i] >= 'A' && expresie[i] <= 'Z'))
+                    ++cnt;
+            return cnt;
+        }
+
+        public int NrCifre(string expresie)
+        {
+
+            int cnt = 0;
+            for (int i = 0; i < expresie.Length; ++i)
+                if (expresie[i] >= '0' && expresie[i] <= '9')
+                    ++cnt;
+            return cnt;
+        }
+        public int NrNumbs(string expresie)
+        {
+
+            int cnt = 0;
+            char[] separators = { ',', '.', ' ', '+', '-', '*', '(', ')' };
+            string[] nr = expresie.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < nr.Length; ++i)
+                if (nr[i][0] >= '0' && nr[i][0] <= '9')
+                    cnt++;
+
+            return cnt;
+        }
+        public void EliminareSpatii(string expresie)
+        {
+
+            char[] sep = { ' ' };
+            string[] nr = expresie.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var x in nr)
+                Console.Write(x + " ");
+        }
+
+        public void EliminareSpatiiSep(string expresie)
+        {
+
+            char[] sep1 = { ' ' };
+            string a = ",";
+            string b = ".";
+            string c = "!";
+            string d = "?";
+            string[] nr = expresie.Split(sep1, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < (nr.Length - 1); ++i)
+                if ((nr[i + 1] != a) && (nr[i + 1] != b) && (nr[i + 1] != c) && (nr[i + 1] != d)) Console.Write(nr[i] + " ");
+                else
+                    Console.Write(nr[i]);
+            int cnt = nr.Length - 1;
+            //if ((nr[cnt] == a) || (nr[cnt] == b) || (nr[cnt] == c) || (nr[cnt] == d))
+            Console.Write(nr[cnt]);
+        }
+
+        public void SortareCuvinte(string expresie)
+        {
+            char[] sep = { ',', '.', ' ', '!', '?', '-', '+', '*', '/', '%', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] nr = expresie.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+            var sir = new ArrayList();
+            foreach (var x in nr)
+                sir.Add(x);
+            sir.Sort();
+            for (int i = 0; i < sir.Count; ++i)
+                Console.Write(sir[i] + " ");
+        }
+        public void SortareNumere(string expresie)
+        {
+            var sir = new ArrayList();
+            char[] separators = { ',', '.', ' ', '+', '-', '*', '(', ')', '/' };
+            string[] nr = expresie.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < nr.Length; ++i)
+                if (nr[i][0] >= '0' && nr[i][0] <= '9')
+                    sir.Add(int.Parse(nr[i]));
+            sir.Sort();
+            for (int i = 0; i < sir.Count; ++i)
+                Console.Write(sir[i] + " ");
+        }
+        public void SortareCuvinteLit(string expresie)
+        {
+            char[] sep = { ',', '.', ' ', '!', '?', '-', '+', '*', '/', '%', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            string[] nr = expresie.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+            var sir = new ArrayList();
+            foreach (var x in nr)
+                sir.Add(int.Parse(x));
+            sir.Sort();
+            for (int i = 0; i < sir.Count; ++i)
+                Console.Write(sir[i] + " ");
+        }
+
+        public void CalculExpresie(string expresie)
+        {
+            DataTable dt = new DataTable();
+            var v = dt.Compute(expresie, "");
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ ProiectInfo/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Evaluare a = new Evaluare();
+            string x = "3 / (22+4)-5 + 2/(1+2)";
+            //string x = Console.ReadLine();
+            Console.WriteLine("Sirul dumneavoastra este:\n" + x);
+            //Console.WriteLine("Sirul dumneavoastra a fost rescris pentru a ocupa mai putin spatiu.\nAcum sirul dumneavoastra arata asa:\n"); a.EliminareSpatii(x); Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Dimensiunea sirului citit este de: " + a.Dimensiune(x));
+            Console.WriteLine();
+            if (a.NrLitere(x) == 0)
+            {
+                Console.WriteLine("Sirul nu contine niciun cuvant.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Nr. de cuvinte al sirului citit este de: " + a.NrCuv(x));
+                Console.WriteLine();
+            }
+            if (a.NrLitere(x) == 0)
+            {
+                Console.WriteLine("Sirul nu contine nicio litera.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Nr.de litere al sirului citit este de: " + a.NrLitere(x));
+                Console.WriteLine();
+            }
+            Console.WriteLine("Nr.de cifre al sirului citit este de: " + a.NrCifre(x));
+            Console.WriteLine();
+            if (a.NrLitere(x) != 0)
+            {
+                Console.WriteLine("In sirul dumneavoastra se afla " + a.NrNumbs(x) + " numere");
+                Console.WriteLine();
+            }
+            if (a.NrLitere(x) > 0 && a.NrCifre(x) == 0)
+            {
+                Console.WriteLine("Am ordonat cuvintele din sir crescator lexicografic: ");
+                a.SortareCuvinte(x);
+                Console.WriteLine();
+            }
+            else if (a.NrLitere(x) == 0 && a.NrCifre(x) > 0)
+            {
+                Console.Write("Am ordonat numerele din sir crescator lexicografic: ");
+                a.SortareNumere(x);
+                Console.WriteLine();
+            }
+            else if (a.NrLitere(x) > 0 && a.NrCifre(x) > 0)
+            {
+                Console.WriteLine("Am ordonat cuvintele din sir crescator lexicografic");
+                a.SortareCuvinte(x);
+                Console.WriteLine();
+                Console.Write("Am ordonat numerele din sir crescator: ");
+                a.SortareNumere(x);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            if (a.NrLitere(x) > 0)
+            {
+                Console.WriteLine("Sirul dumneavoastra a fost rescris.\nDaca acum sirul difera de cel initial inseamna ca la inceput acesta a avut anumite greseli:\n");
+                a.EliminareSpatiiSep(x);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else if (a.NrCifre(x) > 0)
+            {
+                Console.Write("Cum sirul dumneavoastra este reprezentat de o expresie matematica, o vom calcula.\nLa acest calcul participa " + a.NrNumbs(x) + " numere\nRezultatul obtinut este: ");
+                a.CalculExpresie(x);
+                Console.WriteLine();
+            }
+
+            Console.Write("\n\n");
+
+            Evaluare b = new Evaluare();
+            string y = "Astazi voi lua nota mare la     informatica  ,      cel putin      asa sper    , mai iau un 3 la mate     si un 5 la franceza    si sunt boss   .";
+            //string x = Console.ReadLine();
+            Console.WriteLine("Sirul dumneavoastra este:\n" + y);
+            //Console.WriteLine("Sirul dumneavoastra a fost rescris pentru a ocupa mai putin spatiu.\nAcum sirul dumneavoastra arata asa:\n"); a.EliminareSpatii(x); Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Dimensiunea sirului citit este de: " + a.Dimensiune(y));
+            Console.WriteLine();
+            if (a.NrLitere(y) == 0)
+            {
+                Console.WriteLine("Sirul nu contine niciun cuvant.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Nr. de cuvinte al sirului citit este de: " + a.NrCuv(y));
+                Console.WriteLine();
+            }
+            if (a.NrLitere(y) == 0)
+            {
+                Console.WriteLine("Sirul nu contine nicio litera.");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Nr.de litere al sirului citit este de: " + a.NrLitere(y));
+                Console.WriteLine();
+            }
+            Console.WriteLine("Nr.de cifre al sirului citit este de: " + a.NrCifre(y));
+            Console.WriteLine();
+            if (a.NrLitere(y) != 0)
+            {
+                Console.WriteLine("In sirul dumneavoastra se afla " + a.NrNumbs(y) + " numere");
+                Console.WriteLine();
+            }
+            if (a.NrLitere(y) > 0 && a.NrCifre(y) == 0)
+            {
+                Console.WriteLine("Am ordonat cuvintele din sir crescator lexicografic");
+                a.SortareCuvinte(y);
+                Console.WriteLine();
+            }
+            else if (a.NrLitere(y) == 0 && a.NrCifre(y) > 0)
+            {
+                Console.Write("Am ordonat numerele din sir crescator lexicografic: ");
+                a.SortareNumere(y);
+                Console.WriteLine();
+            }
+            else if (a.NrLitere(y) > 0 && a.NrCifre(y) > 0)
+            {
+                Console.WriteLine("Am ordonat cuvintele din sir crescator lexicografic");
+                a.SortareCuvinte(y);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write("Am ordonat numerele din sir crescator: ");
+                a.SortareNumere(y);
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            if (a.NrLitere(y) > 0)
+            {
+                Console.WriteLine("Sirul dumneavoastra a fost rescris.\nDaca acum sirul difera de cel initial inseamna ca la inceput acesta a avut anumite greseli:\n");
+                a.EliminareSpatiiSep(y);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            else if (a.NrCifre(y) > 0)
+            {
+                Console.Write("Cum sirul dumneavoastra este reprezentat de o expresie matematica, o vom calcula.\nLa acest calcul participa " + a.NrNumbs(y) + " numere\nRezultatul obtinut este: ");
+                a.CalculExpresie(y);
+                Console.WriteLine();
+            }
+        }
+    }
+}
